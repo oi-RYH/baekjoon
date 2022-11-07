a0c2f538778a39f09bcd234d2fe290a89988344e
bae <walkair2003@gmail.com>
2022-11-07 13:48:15 +0900

    Initial Commit

---
+++ baekjoon/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  baekjoon
+//
+//  Created by 배정훈 on 2022/11/07.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
